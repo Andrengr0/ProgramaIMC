d79ae729dfee0f832a1ad192ffc7f4b74deb9010
Andrengr0 <andrenegreirosmoreira@gmail.com>
2022-09-28 23:22:24 -0300

    atualização

--- js/functions.js
+++ js/functions.js
@@ -17,22 +17,22 @@ $(()=>{
     function diagnostico(imc){
 
         if(imc < 18.5){
-            var diagnostico = "Você está muito magro!"
+            var diagnostico = "Você está muito magro!<br>Procure uma dieta focada no ganho de peso!"
             return diagnostico;
-        }else if(imc >= 18.5 && imc < 24.9){
-            var diagnostico = "Você está com peso normal!"
+        }else if(imc >= 18.5 && imc < 25){
+            var diagnostico = "Você está com peso normal!<br>Procure manter uma alimentação saudável e realizar atividas físicias regulares!"
             return diagnostico;
-        }else if(imc >= 25 && imc <= 29.9){
-            var diagnostico = "Você está com sobrepeso!"
+        }else if(imc >= 25 && imc < 30){
+            var diagnostico = "Você está com sobrepeso!<br>Procure uma dieta saudável focada na perca de peso e realizar bastante exercício físico regularmente!"
             return diagnostico;
-        }else if(imc > 29.9 && imc < 35){
-            var diagnostico = "Você está com obesidade grau 1!"
+        }else if(imc >= 30 && imc < 35){
+            var diagnostico = "Você está com obesidade grau 1!<br>Procure uma dieta saudável focada na perca de peso e realizar bastante exercício físico regularmente, procure também um nutricionista!"
             return diagnostico;
         }else if(imc >= 35 && imc < 40){
-            var diagnostico = "Você está com obesidade grau 2!"
+            var diagnostico = "Você está com obesidade grau 2!<br>Procure uma dieta saudável focada na perca de peso e realizar bastante exercício físico regularmente, procure também um nutricionista!"
             return diagnostico;
         }else if(imc >= 40 && imc < 45){
-            var diagnostico = "Você está com obesidade grau 3!"
+            var diagnostico = "Você está com obesidade grau 3!<br>Procure uma dieta saudável focada na perca de peso e realizar bastante exercício físico regularmente, procure também um nutricionista!"
             return diagnostico;
         }else{
             var diagnostico = "Informação incorreta, tente novamente!"
@@ -60,7 +60,7 @@ $(()=>{
         $('.card2 p').css('font-size','20px').css('color','#ffffff');
 
         var mostrarDiagnostico = $('.card2').append("<p>"+diagnostico(resultIMC())+"</p>")
-        $('.card2 p').css('font-size','20px').css('color','#ffffff').css('margin-top','15px');
+        $('.card2 p').css('font-size','20px').css('color','#ffffff').css('margin-top','15px').css('line-height','25px');
 
     })
 
